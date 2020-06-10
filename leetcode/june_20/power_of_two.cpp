2250dadc09ad18eadaa09f1132527bcfe3ce58f9
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-06-10 00:57:16 -0500

    .

---
+++ leetcode/june_20/power_of_two.cpp
@@ -0,0 +1,27 @@
+/* *>>>>>Anshit_Bhardwaj<<<<<* */
+#include <bits/stdc++.h>
+#define ll long long
+#define f(i, x, n) for(int i = x; i < n; i++)
+#define dbg(x) cout << x << endl
+#define dbg2(x, y) cout << x << ' ' << y << endl
+#define dbg3(x, y, z) cout << x << ' ' << y << ' ' << z << endl
+#define mod 1000000007
+using namespace std;
+
+bool isPowerOfTwo(int n) {
+    if(n == 0 or n < 0){
+        return false;
+    }
+    return !(n&(n-1));
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T, n;
+    cin >> T;
+    while(T--){
+        cin >> n;
+        
+    }
+}

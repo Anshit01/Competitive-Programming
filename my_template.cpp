e78926bb8336a097c74b6867550383b27d6fb854
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-08-11 23:45:14 +0530

    updated template

--- my_template.cpp
+++ my_template.cpp
@@ -1,19 +1,22 @@
 /* *>>>>>Anshit_Bhardwaj<<<<<* */
 #include <bits/stdc++.h>
 #define ll long long
-#define f(i, x, n) for(int i = x; i < n; i++)
-#define dbg(x) cout << x << endl
-#define dbg2(x, y) cout << x << ' ' << y << endl
-#define dbg3(x, y, z) cout << x << ' ' << y << ' ' << z << endl
+//#define int long long
+#define f(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define dbg(x) cerr << x << endl
+#define dbg2(x, y) cerr << x << ' ' << y << endl
+#define dbg3(x, y, z) cerr << x << ' ' << y << ' ' << z << endl
 #define mod 1000000007
 using namespace std;
 
-int main(){
+int32_t main(){
     ios::sync_with_stdio(0);
     cin.tie(NULL);
-    int T, n;
+    int T;
     cin >> T;
     while(T--){
+        int n;
         cin >> n;
         
     }

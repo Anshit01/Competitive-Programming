41a4b453e4eb10ff0ad3f84df5375c116724ebce
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-08-11 10:31:54 +0530

    added debug command for g++

--- test.cpp
+++ test.cpp
@@ -1,32 +1,19 @@
-#include <stdio.h>
-#define SIZE 1000
-int Stack[1000], top = -1;
-
-void push(char c){
-    if(top < SIZE){
-        top++;
-        Stack[top] = c;
-    }
-}
-
-char pop(){
-    if(top != -1){
-        return Stack[top--];
-    }
-}
+/* *>>>>>Anshit_Bhardwaj<<<<<* */
+#include <bits/stdc++.h>
+#define ll long long
+#define f(i, x, n) for(int i = x; i < n; i++)
+#define dbg(x) cout << x << endl
+#define dbg2(x, y) cout << x << ' ' << y << endl
+#define dbg3(x, y, z) cout << x << ' ' << y << ' ' << z << endl
+#define mod 1000000007
+using namespace std;
 
 int main(){
-    char str[100];
-    scanf("%s", str);
-    int i = 0;
-    while(str[i] != '\0'){
-        push(str[i]);
-        i++;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    vector<int> arr(5);
+    f(i, 0, 6){
+        cout << arr[i] << endl;
     }
-    i = 0;
-    while(str[i] != '\0'){
-        str[i] = pop();
-        i++;
-    }
-    printf("%s", str);
+    cout << endl;
 }

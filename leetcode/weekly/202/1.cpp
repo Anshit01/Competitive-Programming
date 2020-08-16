780c26d05503f4c0d02a8e6aa5b0107e6cee2fc1
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-08-16 08:42:36 +0530

    .

---
+++ leetcode/weekly/202/1.cpp
@@ -0,0 +1,49 @@
+/* *>>>>>Anshit_Bhardwaj<<<<<* */
+#include <bits/stdc++.h>
+#define ll long long
+//#define int long long
+#define f(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define what_is(x) cerr << #x << " is " << x << endl
+#define dbg(x) cerr << x << endl
+#define dbg2(x, y) cerr << x << ' ' << y << endl
+#define dbg3(x, y, z) cerr << x << ' ' << y << ' ' << z << endl
+#define inputArray(arr) f(i, 0, arr.size()) cin >> arr[i]
+#define printArray(arr) f(i, 0, arr.size()) cout << arr[i] << ' '; cout << endl
+#define mod 1000000007
+using namespace std;
+
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int odd = 0;
+        for(int i = 0; i < arr.size(); i++){
+            if(odd > 0){
+                if(arr[i] % 2 == 0){
+                    odd = 0;
+                }else{
+                    odd++;
+                    if(odd == 3){
+                        return true;
+                    }
+                }
+            }else{
+                if(arr[i] % 2 == 1){
+                    odd++;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while(T--){
+        int n;
+        cin >> n;
+        
+    }
+}
---
+++ leetcode/weekly/202/2.cpp
@@ -0,0 +1,44 @@
+/* *>>>>>Anshit_Bhardwaj<<<<<* */
+#include <bits/stdc++.h>
+#define ll long long
+//#define int long long
+#define f(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define what_is(x) cerr << #x << " is " << x << endl
+#define dbg(x) cerr << x << endl
+#define dbg2(x, y) cerr << x << ' ' << y << endl
+#define dbg3(x, y, z) cerr << x << ' ' << y << ' ' << z << endl
+#define inputArray(arr) f(i, 0, arr.size()) cin >> arr[i]
+#define printArray(arr) f(i, 0, arr.size()) cout << arr[i] << ' '; cout << endl
+#define mod 1000000007
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(int n) {
+        int l = n/2;
+        int a;
+        if(n % 2 == 1){
+            a = 2;
+        }else{
+            a = 1;
+        }
+        long long sum = 0;
+        for(int i = 0; i < l; i++){
+            sum += a;
+            a += 2;
+        }
+        return sum;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while(T--){
+        int n;
+        cin >> n;
+        
+    }
+}

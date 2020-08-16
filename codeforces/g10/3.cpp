67ff59dd50141f78b54611ffab34410815a5e43e
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-08-17 00:15:36 +0530

    .

---
+++ codeforces/g10/3.cpp
@@ -0,0 +1,32 @@
+/* *>>>>>Anshit_Bhardwaj<<<<<* */
+#include <bits/stdc++.h>
+#define ll long long
+//#define int long long
+#define f(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define what_is(x) cerr << #x << " is " << x << endl
+#define dbg(x) cerr << x << endl
+#define dbg2(x, y) cerr << x << ' ' << y << endl
+#define dbg3(x, y, z) cerr << x << ' ' << y << ' ' << z << endl
+#define inputArray(arr) f(i, 0, arr.size()) cin >> arr[i]
+#define printArray(arr) f(i, 0, arr.size()) cout << arr[i] << ' '; cout << endl
+#define mod 1000000007
+using namespace std;
+
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while(T--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        inputArray(arr);
+        int max_so_far = 0, max_diff = 0;
+        f(i, 0, n){
+            max_so_far = max(max_so_far, arr[i]);
+            max_diff = max(max_diff, max_so_far - arr[i]);
+        }
+        cout << max_diff << endl;
+    }
+}
